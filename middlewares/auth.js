7ad26b0602cb383fa1648253f7165eed3224192b
Mara Krukovskaya <krukovmargaery@yandex.ru>
2021-11-01 00:20:43 +0300

    1st api try auth hotfixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth_error');
 
 const JWT_SECRET = 'secret';
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTdiZDI3YWYzMjgzODBkNGVlYTdjYzgiLCJpYXQiOjE2MzU1MDQ3NzAsImV4cCI6MTYzNjEwOTU3MH0.F_9vyWpZ2hAzcz_WfVvTen3eCa5aoDYX4sVtz5fH44g"
+//const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTdiZDI3YWYzMjgzODBkNGVlYTdjYzgiLCJpYXQiOjE2MzU1MDQ3NzAsImV4cCI6MTYzNjEwOTU3MH0.F_9vyWpZ2hAzcz_WfVvTen3eCa5aoDYX4sVtz5fH44g"
 
 function auth(req, res, next) {
-  //const token = req.cookies.jwt;
-  //console.log({token})
+  const token = req.cookies.jwt;
+  // console.log(req.cookies)
   // console.log(token); // {token: undefined}
   let payload;
 
